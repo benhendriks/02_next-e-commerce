1c7bafdfef48986820e2b951d420b0535b2adf7d
benhendriks <benhendriks@protonmail.com>
2023-07-23 20:32:17 +0200

    Added styling to home

--- app/componenents/Product.tsx
+++ app/componenents/Product.tsx
@@ -1,11 +1,21 @@
 import Image from "next/image";
+import formatPrice from "@/util/PriceFormat";
+import { ProductType } from "@/types/ProductTypes";
 
-export default function Product({ name, image, price }) {
+export default function Product({ name, image, price }: ProductType) {
   return (
-    <div>
-      <Image src={image} alt={name} width={200} height={200} />
-      <h1>{name}</h1>
-      <p>{price}</p>
+    <div className="text-gray-700">
+      <Image
+        className="rounded-lg"
+        src={image}
+        alt={name}
+        width={800}
+        height={800}
+      />
+      <div className="font-medium py-2">
+        <h1>{name}</h1>
+        <h2>{price !== null ? formatPrice(price as number) : "N/A"}</h2>
+      </div>
     </div>
   );
 }
