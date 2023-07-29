2c4355601ad8dfafd4c7dabe38cbf506e7ab478e
benhendriks <benhendriks@protonmail.com>
2023-07-29 16:33:52 +0200

    Added fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,19 @@ import Nav from "./componenents/Nav";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import Hydrate from "./componenents/Hydrate";
+import { Roboto, Lobster_two } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-robot",
+});
+
+const lobster = Roboto({
+  weight: "700",
+  subsets: ["latin"],
+  variable: "--font-lobster",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -18,7 +31,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className="mx-64">
+      <body className={`mx-64 ${roboto.className}`}>
         <Hydrate>
           <Nav user={session?.user} expires={session?.expires as string} />
           {children}
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,6 +8,7 @@ const prisma = new PrismaClient();
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
@@ -15,22 +16,22 @@ export const authOptions = {
     }),
   ],
   events: {
-    createUser: async ({user}) => {
-       const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-        apiVersion: '2022-11-15'
-       })
-       if(user.name && user.email) {
-         const customer = await stripe.customers.create({
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+        apiVersion: "2022-11-15",
+      });
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
           email: user.email,
-          name: user.name,  
-         })
-         await prisma.user.update({
-           where: { id: user.id },
-           data: { stripeCustomerId: customer.id}
-         })
-       }
+          name: user.name,
+        });
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeCustomerId: customer.id },
+        });
+      }
     },
   },
 };
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
